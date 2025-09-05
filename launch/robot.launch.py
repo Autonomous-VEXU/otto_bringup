65ae4b162cfefe315504548c03d222ce760f4584
kmhswimgirl <kmherbstzuber@wpi.edu>
2025-09-05 03:47:51 -0400

    launch file works
    need to fix the wheels on the robot soon

--- launch/robot.launch.py
+++ launch/robot.launch.py
@@ -1,41 +1,57 @@
-#!/bin/usr/python
-
-# paths to urdf, 
-# need param called robot_description to 
-# robot state publisher
-# joint state publisher
-
-import launch
-from launch.substitutions import Command, LaunchConfiguration
-import launch_ros
+#!/usr/bin/env python3
 import os
+import launch
+from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-    pkg_share = launch_ros.substitutions.FindPackageShare(package='vex_robot').find('vex_robot')
-    default_model_path = os.path.join(pkg_share, 'urdf/x-drive.urdf.xacro')
-    #default_rviz_config_path = os.path.join(pkg_share, 'rviz/config.rviz')
 
-    robot_state_publisher = launch_ros.actions.Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}]
+    default_model_path = PathJoinSubstitution([
+        FindPackageShare('vex_robot'),
+        'urdf',
+        'x-drive.urdf.xacro'
+    ])
+    
+    # Rviz configuration file 
+    rviz_config = os.path.join(
+        get_package_share_directory('vex_robot'), 
+        'rviz', 
+        'robot_visualizer.rviz'
     )
-    joint_state_publisher = launch_ros.actions.Node(
-        package='joint_state_publisher',
-        executable='joint_state_publisher',
-        name='joint_state_publisher',
-    )
-    rviz_node = launch_ros.actions.Node(
+
+    rviz = Node(
         package='rviz2',
         executable='rviz2',
         name='rviz2',
-        output='screen'
+        output='screen',
+        arguments=['-d', rviz_config]
     )
-
+    
+    robot_state = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output="screen",
+        parameters=[{
+            'robot_description': Command(['xacro ', LaunchConfiguration('model')])
+        }]
+    )
+    
+    joints = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+        output="screen"
+    )
+    
     return launch.LaunchDescription([
-        launch.actions.DeclareLaunchArgument(name='model', default_value=default_model_path,
-                                            description='Absolute path to robot urdf file'),
-        joint_state_publisher,
-        robot_state_publisher,
-        rviz_node
+        launch.actions.DeclareLaunchArgument(
+            name='model',
+            default_value=default_model_path,
+            description='path to robot model'
+        ),
+        joints,
+        robot_state,
+        rviz
     ])
