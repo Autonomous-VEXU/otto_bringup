65c3dbea008600061493ef39060646d803d212ea
kmhswimgirl <kmherbstzuber@wpi.edu>
2025-09-05 19:20:55 -0400

    it builds!

--- launch/robot.launch.py
+++ launch/robot.launch.py
@@ -1,63 +1,70 @@
 #!/usr/bin/env python3
+
 import os
-import launch
-from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
-from launch_ros.substitutions import FindPackageShare
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, RegisterEventHandler
+from launch.event_handlers import OnProcessExit
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument
-from ament_index_python.packages import get_package_share_directory
+from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
+    robot_description_content = Command([
+        PathJoinSubstitution([FindExecutable(name="xacro")]),
+        " ",
+        PathJoinSubstitution([
+            FindPackageShare("vex_robot"),
+            "urdf",
+            "x_drive.urdf.xacro"
+        ])
+    ])
+    
+    robot_description = {"robot_description": robot_description_content}
 
-    default_model_path = PathJoinSubstitution([
-        FindPackageShare('vex_robot'),
-        'urdf',
-        'x_drive.urdf.xacro'
+    controller_config = PathJoinSubstitution([
+        FindPackageShare("vex_robot"),
+        "config",
+        "omni_wheel_params.yaml"
     ])
 
-    model = DeclareLaunchArgument(
-            name='model',
-            default_value=default_model_path,
-            description='path to robot model'
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="both",
+        parameters=[robot_description],
+    )
+
+    controller_manager_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[robot_description, controller_config],
+        output="both",
+    )
+
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
+    )
+
+    omni_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["omni_wheel_drive_controller", "--controller-manager", "/controller_manager"],
+    )
+
+    delay_omni_controller_spawner_after_joint_state_broadcaster = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=joint_state_broadcaster_spawner,
+            on_exit=[omni_controller_spawner],
         )
-    
-    # Rviz configuration file 
-    rviz_config = os.path.join(
-        get_package_share_directory('vex_robot'), 
-        'rviz', 
-        'robot_visualizer.rviz'
     )
 
-    rviz = Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz2',
-        output='screen',
-        arguments=['-d', rviz_config]
-    )
-    
-    robot_state = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        output="screen",
-        parameters=[{
-            'robot_description': Command(['xacro ', LaunchConfiguration('model')])
-        }]
-    )
-    
-    joints = Node(
-        package='joint_state_publisher',
-        executable='joint_state_publisher',
-        name='joint_state_publisher',
-        output="screen",
-        parameters=[{
-            'robot_description': Command(['xacro ', LaunchConfiguration('model')])
-        }]
-    )
-    
-    return launch.LaunchDescription([
-        model,
-        joints,
-        robot_state,
-        rviz
-    ])
+    nodes = [
+        robot_state_publisher_node,
+        controller_manager_node,
+        joint_state_broadcaster_spawner,
+        delay_omni_controller_spawner_after_joint_state_broadcaster,
+    ]
+
+    return LaunchDescription(nodes)
