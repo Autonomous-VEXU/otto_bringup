42f55d53a090d538a7127fd44487e9e1af36d002
kmhswimgirl <kmherbstzuber@wpi.edu>
2025-09-12 18:49:31 -0400

    CMakeLists + package.xml for all pkgs

--- robot_bringup/launch/rviz2.launch.py
+++ robot_bringup/launch/rviz2.launch.py
@@ -5,7 +5,6 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
-
 def generate_launch_description():
     rviz_config_dir = os.path.join( # get path to Rviz2 config file
         get_package_share_directory('robot_description'),
