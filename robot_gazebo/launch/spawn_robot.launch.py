bb70637df59ad33ae72481c4e4479aec8deab05b
kmhswimgirl <kmherbstzuber@wpi.edu>
2025-09-18 01:48:38 -0400

    finally, a different error message

--- robot_gazebo/launch/spawn_robot.launch.py
+++ robot_gazebo/launch/spawn_robot.launch.py
@@ -3,43 +3,54 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, RegisterEventHandler, TimerAction
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
+from launch.event_handlers import OnProcessStart
 import xacro
 
-
 def generate_launch_description():
-    # Get the urdf file
-
+   
+    # URDF/ file path stuff
     urdf_path = os.path.join(
-        get_package_share_directory('robot_description'),
-          'urdf',
+        get_package_share_directory('robot_gazebo'),
+          'models',
         'x_drive.urdf.xacro'
     )
 
     urdf = xacro.process_file(urdf_path).toxml()
 
-    robot_state_publisher_node = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        parameters=[{
-            'robot_description': urdf
-        }]
+    bridge_params = os.path.join(
+        get_package_share_directory('robot_gazebo'),
+        'config',
+        'x_drive_bridge.yaml'
     )
 
-    # Launch configuration variables specific to simulation
+    controllers_params = os.path.join(
+        get_package_share_directory('robot_gazebo'),
+        'config',
+        'omni_wheel_params.yaml'
+    )
+
     x_pose = LaunchConfiguration('x_pose', default='0.0')
     y_pose = LaunchConfiguration('y_pose', default='0.0')
 
-    # Declare the launch arguments
     declare_x_position_cmd = DeclareLaunchArgument(
         'x_pose', default_value='0.0',
         description='Specify namespace of the robot')
 
     declare_y_position_cmd = DeclareLaunchArgument(
         'y_pose', default_value='0.0',
-        description='Specify namespace of the robot')
+        description='Specify namespace of the robot') 
+
+    # robot_state_publisher node
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[{'robot_description': urdf},
+                    {'use_sim_time': True} ]
+    ) 
 
     start_gazebo_ros_spawner_cmd = Node(
         package='ros_gz_sim',
@@ -54,20 +65,10 @@ def generate_launch_description():
         output='screen',
     )
 
-    bridge_params = os.path.join(
-        get_package_share_directory('robot_gazebo'),
-        'config',
-        'x_drive_bridge.yaml'
-    )
-
     start_gazebo_ros_bridge_cmd = Node(
         package='ros_gz_bridge',
         executable='parameter_bridge',
-        arguments=[
-            '--ros-args',
-            '-p',
-            f'config_file:={bridge_params}',
-        ],
+        parameters=[bridge_params],
         output='screen',
     )
 
@@ -77,15 +78,44 @@ def generate_launch_description():
         arguments=['/camera/image_raw'],
         output='screen',
     )
-    ld = LaunchDescription([robot_state_publisher_node])
+
+    joint_state_broadcaster_spawner = TimerAction(
+        period=6.0,
+        actions=[
+            Node(
+                package='controller_manager',
+                executable='spawner',
+                arguments=['joint_state_broadcaster', '--controller-manager', '/controller_manager'],
+                parameters=[{'use_sim_time': True}],
+                output='screen'
+            )
+        ]
+    )
+
+    omni_controller_spawner = TimerAction(
+        period=8.0,
+        actions=[
+            Node(
+                package='controller_manager',
+                executable='spawner',
+                arguments=['omni_wheel_drive_controller', '--controller-manager', '/controller_manager'],
+                parameters=[{'use_sim_time': True}],
+                output='screen'
+            )
+        ]
+    )
+
+    ld = LaunchDescription([
+        start_gazebo_ros_spawner_cmd,
+        start_gazebo_ros_bridge_cmd,
+        start_gazebo_ros_image_bridge_cmd,
+        robot_state_publisher_node,
+        joint_state_broadcaster_spawner,
+        omni_controller_spawner
+        ])
 
     # Declare the launch options
     ld.add_action(declare_x_position_cmd)
     ld.add_action(declare_y_position_cmd)
-
-    # Add any conditioned actions
-    ld.add_action(start_gazebo_ros_spawner_cmd)
-    ld.add_action(start_gazebo_ros_bridge_cmd)
-    ld.add_action(start_gazebo_ros_image_bridge_cmd)
 
     return ld
