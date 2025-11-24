c253a05dc3e3c4b3c558775a4eef41871c708bd4
kmhswimgirl <kmherbstzuber@wpi.edu>
2025-11-24 15:46:01 -0500

    dual lidar in correct frame

--- robot_bringup/scripts/lidar_bringup.bash
+++ robot_bringup/scripts/lidar_bringup.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # launch both lidars
-ros2 launch sick_scan_xd sick_tim_7xxS.launch.py hostname:=192.168.0.51 __ns:=/right frame_id:=laser_frame_1 & \
-ros2 launch sick_scan_xd sick_tim_7xxS.launch.py hostname:=192.168.0.50 __ns:=/left frame_id:=laser_frame_2
+ros2 launch sick_scan_xd sick_tim_7xxS.launch.py hostname:=192.168.0.51 __ns:=/right frame_id:=r_laser tf_base_frame_id:=laser_frame_2 & \
+ros2 launch sick_scan_xd sick_tim_7xxS.launch.py hostname:=192.168.0.50 __ns:=/left frame_id:=l_laser tf_base_frame_id:=laser_frame_1
 
