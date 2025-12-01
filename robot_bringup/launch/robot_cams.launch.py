52fe6d973668db3b7451bc7316eb91732d5edf98
kmhswimgirl <kmherbstzuber@wpi.edu>
2025-12-01 14:38:51 -0500

    cameras wip, lidar is done

--- robot_bringup/launch/robot_cams.launch.py
+++ robot_bringup/launch/robot_cams.launch.py
@@ -3,17 +3,24 @@
 import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import TimerAction
 from ament_index_python import get_package_share_directory
 
 def generate_launch_description():
     pkg_dir= get_package_share_directory('robot_bringup')
     config_file = os.path.join(pkg_dir, 'config', 'camera_params.yaml')
-    
+
     camera_1 = Node( 
         package='usb_cam', 
         executable='usb_cam_node_exe', 
         output='screen',
         name='camera_1',
+        remappings=[
+            ('/image_raw', 'cam1/image_raw'),
+            ('/camera_info', 'cam1/camera_info'),
+            ('image_raw/compressed', 'cam1/image_raw/compressed')
+        ],
+        #namespace='c1',
         parameters=[config_file]
     ) 
     
@@ -22,6 +29,13 @@ def generate_launch_description():
         executable='usb_cam_node_exe', 
         output='screen',
         name='camera_2',
+        remappings=[
+            ('/image_raw', 'cam2/image_raw'),
+            ('/camera_info', 'cam2/camera_info'),
+            ('image_raw/compressed', 'cam2/image_raw/compressed')
+        ],
+
+        #namespace='c2',
         parameters=[config_file]
     )  
 
@@ -30,6 +44,11 @@ def generate_launch_description():
         executable='usb_cam_node_exe', 
         output='screen',
         name='camera_3',
+        remappings=[
+                ('/image_raw', 'cam3/image_raw'),
+                ('/camera_info', 'cam3/camera_info'),
+                ('image_raw/compressed', 'cam3/image_raw/compressed') 
+            ],
         parameters=[config_file] 
     )  
     
@@ -38,12 +57,33 @@ def generate_launch_description():
         executable='usb_cam_node_exe', 
         output='screen',
         name='camera_4',
+        remappings=[
+                ('/image_raw', 'cam4/image_raw'),
+                ('/camera_info', 'cam4/camera_info'),
+                ('image_raw/compressed', 'cam4/image_raw/compressed') 
+            ],
         parameters=[config_file]
     )  
     
+
+    timer_2 = TimerAction( 
+        period=2.0,
+        actions=[camera_2]
+    )
+
+    timer_3 = TimerAction(
+        period=5.0,
+        actions=[camera_3]
+    )
+
+    timer_4 = TimerAction(
+        period=8.0,
+        actions=[camera_4]
+    )
+    
     return LaunchDescription([
         camera_1,
-        camera_2,
-        camera_3,
-        camera_4
+        timer_2,
+        timer_3,
+        timer_4
     ])
