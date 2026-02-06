68f9d714ded8bca418aa9ab5ca832f41da253bb7
kmhswimgirl <kmherbstzuber@wpi.edu>
2026-02-06 01:21:25 -0500

    disable scan merger

--- launch/robot.launch.py
+++ launch/robot.launch.py
@@ -17,7 +17,7 @@ def generate_launch_description():
 
     # package directories
     pkg_dir = get_package_share_directory('otto_bringup')
-    scan_merger_pkg = get_package_share_directory('laser_scan_merger')
+    # scan_merger_pkg = get_package_share_directory('laser_scan_merger')
 
     # camera launch argument
     launch_cams = LaunchConfiguration('cams')
@@ -171,13 +171,13 @@ def generate_launch_description():
     )
 
     # laser_scan_merger launch file
-    scan_merger = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(scan_merger_pkg, 'launch', 'start.launch.py')
-        ),
-        launch_arguments={'robotname':'x_drive'}.items(),
-        condition=IfCondition(launch_lidar)
-    )
+    # scan_merger = IncludeLaunchDescription(
+    #     PythonLaunchDescriptionSource(
+    #         os.path.join(scan_merger_pkg, 'launch', 'start.launch.py')
+    #     ),
+    #     launch_arguments={'robotname':'x_drive'}.items(),
+    #     condition=IfCondition(launch_lidar)
+    # )
 
     # camera bringup
     robot_cams = IncludeLaunchDescription(
@@ -200,6 +200,6 @@ def generate_launch_description():
         misc_sensors,
         right_lidar,
         left_lidar,
-        scan_merger,
+        #scan_merger,
         robot_cams
     ])
